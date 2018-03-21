2caeb38b051c14313f7243db7a14e24e1089d378
YoDevil <alessandro.astone@hotmail.com>
2018-03-21 21:02:51 +0100

    Use process.env.NODE_ENV to detect debugging environment

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const __PATH__ = (window.location.hostname === "andreafranchini.me") ?
-    "/dnd-spell-cards-online" : "";
+const __PATH__ = (process.env.NODE_ENV === "development") ?
+    "" : "/dnd-spell-cards-online";
 
 ReactDOM.render((
     <BrowserRouter basename={__PATH__ + "/"}>
